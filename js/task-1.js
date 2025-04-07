6c2e1c83154b669b567f8bd99c1b878c3a7954d2
Stanislav Porohnavets <stasporoxnavec@gmail.com>
2025-04-07 20:42:16 +0300

    hw5

--- js/task-1.js
+++ js/task-1.js
@@ -1,66 +1,41 @@
-// function makeTransaction(quantity, pricePerDroid) {
-//     const totalPrice = quantity * pricePerDroid;
-//     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-// }
-// console.log(makeTransaction(5, 3000)); 
-// console.log(makeTransaction(3, 1000)); 
-// console.log(makeTransaction(10, 500)); 
-
-
-// function makeTransaction(quantity, pricePerDroid, customerCredits) {
-//     const totalPrice = quantity * pricePerDroid;
-  
-//     if (totalPrice > customerCredits) {
-//       return "Insufficient funds!";
-//     } else {
-//       return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-//     }
-//   }
-  
-//   console.log(makeTransaction(5, 3000, 23000));
-//   console.log(makeTransaction(3, 1000, 15000));
-//   console.log(makeTransaction(10, 5000, 8000)); 
-//   console.log(makeTransaction(8, 2000, 10000)); 
-//   console.log(makeTransaction(10, 500, 5000)); 
-  
-
-
-
-// function slugify(title) {
-
-//   return title.toLowerCase().split(' ').join('-');
-// }
-
-
-// console.log(slugify("Arrays for beginners"));  
-// console.log(slugify("English for developer")); 
-// console.log(slugify("Ten secrets of JavaScript"));
-// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
-
-
-
-
-function isEnoughCapacity(products, containerSize) {
-  const totalQuantity = Object.values(products).reduce((sum, quantity) => sum + quantity, 0);
-  
-  return totalQuantity <= containerSize;
-}
-
+const getUserNames = users => users.map(user => user.name);
 
 console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); 
-
-console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-); 
-
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-); 
-
-console.log(
-  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-); 
-
-
+  getUserNames([
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      balance: 2811
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      balance: 3821
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      balance: 3793
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      balance: 2278
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      balance: 3951
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      balance: 1498
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      balance: 2764
+    },
+  ])
+);
